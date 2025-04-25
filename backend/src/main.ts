7292b36cb599fa36ffdad6546055ee5064d9a75b
caio-ferreira-dev <caioferreirasdj@gmail.com>
2025-04-25 13:40:32 -0300

    added(nestjs backend setup)

---
+++ backend/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // CORS
+  app.enableCors();
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API template')
+    .setDescription('NestJS api template')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(`[🤖]: Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
